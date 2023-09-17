8b2a84aa94d370c30e874a1c4133276e475a6dc5
dgomezqu <32272595+dgomezqu@users.noreply.github.com>
2023-09-17 15:22:56 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_data():
+    try:
+        conn = psycopg2.connect(
+            database="mydb",
+            user="myuser",
+            password="mypassword",
+            host="postgresql",
+            port="5432",
+        )
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM your_table")
+        data = cursor.fetchall()
+
+        return str(data)
+    except Exception as e:
+        return str(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
